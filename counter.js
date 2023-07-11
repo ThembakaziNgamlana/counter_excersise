6b122e011280f79e9749c8cd6176510f65bbeff2
Thembakazi <thembakazingamlana25@gmail.com>
2023-07-11 15:03:45 +0200

    adding files

---
+++ counter.js
@@ -0,0 +1,18 @@
+
+
+
+const counter1 = Counter();
+const counter2 = Counter();
+
+counter1.increment();
+counter1.increment();
+counter1.increment();
+
+counter2.increment();
+counter2.increment();
+
+// prints 3 
+console.log(counter1.count);
+
+//prints 2
+console.log(counter2.count);
